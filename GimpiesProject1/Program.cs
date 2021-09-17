6eb44d2e96d0eed4f88c5fe144db3047ed020a6b
Jan Paul van der Meulen <00253791@rijnijssel.nl>
2021-09-17 13:00:51 +0200

    Add project files.

---
+++ GimpiesProject1/Program.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimpiesProject1
+{
+    class Program
+    {
+        
+
+        static void MainMenu()
+        {
+            
+                
+                
+            
+                
+
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("1. Voorraad Schoenen bejiken");
+                Console.WriteLine("2. Schoenen Inkopen");
+                Console.WriteLine("3. Uitloggen.");
+                int MenuOptions = Convert.ToInt32(Console.ReadLine());
+                       
+            switch (MenuOptions)
+            {
+                case 1:
+                    {
+                        MenuVoorraad();
+                        break;
+                    }
+                case 2:
+                    {
+                        MenuInkopen();
+                        break;
+                    }
+                case 3:
+                    {
+                        MenuUitloggen();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("That's not an option. ");
+                        //Console.Read();
+                        MainMenu();
+                        Console.WriteLine("That's not an option. ");
+                        break;
+                    }                
+                }
+            }
+            catch (FormatException)
+            {
+                MainMenu();
+            }
+        }
+        //Voorraad voor Schoenen bekijken.
+        static void MenuVoorraad()
+        {
+            Shoes[] S = new Shoes[2];
+            {
+                S[0] = new Shoes();
+                S[0].SetShoes("Nike", "Air Max", 42, "Rood", 12, 49.99);
+                S[0].printShoes();
+                Console.ReadLine();
+            };
+
+            try
+            {                
+                
+            Console.WriteLine("Om terug te gaan, druk x");
+            
+
+            string Goback = Console.ReadLine();
+            if (Goback == "x")
+            {
+                MainMenu();
+            }
+            }catch (FormatException)
+            {
+                MainMenu();
+            }
+
+        }
+
+        static void MenuInkopen()
+        {
+            Shoes[] S = new Shoes[2];
+            {
+                S[0] = new Shoes();
+                S[0].SetShoes("Nike", "Air Max", 42, "Rood", 12, 49.99);
+                S[0].printShoes();
+                Console.ReadLine();
+            };
+            Console.Clear();
+            Console.WriteLine("Wilt u iets toepassen? Druk op Y of X.");
+            string inputCheck = Console.ReadLine();
+            if (inputCheck == "Y")
+            {
+            Console.Write("Wat voor Merk: ");
+            string input1 = Console.ReadLine();
+            Console.Write("Wat voor type: ");
+            string input2 = Console.ReadLine();
+            Console.Write("Wat voor size: ");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wat voor kleur: ");
+            string input4 = Console.ReadLine();
+            Console.Write("Hoeveel: ");
+            int input5 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wat is de prijs: ");
+            double input6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input1 + input2 + input3 + input4 + input5 + input6);
+            Shoes[] S2 = new Shoes[2];
+            S2[1].SetShoes(input1, input2, input3, input4, input5, input6);
+            S2[1].printShoes();
+            Console.WriteLine("Klopt dit gegevens?");
+            string inputInkopen = Console.ReadLine();
+            if (inputInkopen == "Y")
+            {
+                S2[1].printShoes();
+                Console.ReadLine();
+                MainMenu();
+            }
+            else
+                Console.WriteLine("Opnieuw proberen.");
+            Console.ReadLine();
+            }
+        }
+
+        static void MenuUitloggen()
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+            ;
+
+        }
+
+        static void MenuAdmin()
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            var arrUsers = new Users[]
+                {
+            new Users("user1","valid1",1),
+            new Users("user2","valid2",2),
+            new Users("user3","valid3",3)
+                };
+            int loginAttempts = 0;
+            bool succesfull = false;
+            while (!succesfull)
+            {   
+                if (loginAttempts == 3)
+                {
+                    Console.Clear();
+                    Console.Write("You've attempted to log in too many times, This application will now close.");
+                    Console.ReadLine();
+                    Environment.Exit(-1);
+                }
+
+                Console.WriteLine("Login Screen");
+                Console.Write("Username: ");
+                var username = Console.ReadLine();
+                Console.Write("Password: ");
+                var password = ReadPassword();  
+                Console.WriteLine();
+                
+            
+                foreach (Users user in arrUsers)
+                {
+                    if (username == user.username && password == user.password)
+                    {
+                    Console.WriteLine("Je bent succesvol ingelogd!!!");
+                    Console.ReadLine();
+                    succesfull = true;
+                    break;
+                    }
+                }
+                if (!succesfull)
+                {
+                    Console.WriteLine("Your username or password is incorect, try again !!!");
+
+                    loginAttempts++;
+                    Console.WriteLine("login attempts: " + loginAttempts + " of 3");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            MainMenu();
+        }
+
+        //Password Masking
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
+        }
+
+       /* //This will create a table.
+        static int tableWidth = 85;
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }*/
+    }  
+    
+}
+public class Users
+{
+    public string username;
+    public string password;
+    public int id;
+
+    public Users(string username, string password, int id)
+    {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+    }
+}
+
+public class Shoes
+{
+    public string merk;
+    public string type;
+    public int size;
+    public string kleur;
+    public int aantal;
+    public double prijs;
+
+    public void SetShoes(string merk, string type, int size, string kleur, int aantal, double prijs)
+    {
+        this.merk = merk;
+        this.type = type;
+        this.size = size;
+        this.kleur = kleur;
+        this.aantal = aantal;
+        this.prijs = prijs;
+    }
+    public void printShoes()
+    {
+        Console.WriteLine("Inventory 1  : ");
+        Console.WriteLine("\tMerk       : " + merk);
+        Console.WriteLine("\tType       : " + type);
+        Console.WriteLine("\tSize       : " + size);
+        Console.WriteLine("\tKleur      : " + kleur);
+        Console.WriteLine("\tAantal     : " + aantal);
+        Console.WriteLine("\tPrijs      : " + prijs);
+    }
+
+}
+
+
+
+
